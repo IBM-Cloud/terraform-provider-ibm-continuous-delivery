f0c0805f26a09af66ac19d4e3fe024b0f6a841ac
Gary Marjoram <marjoram@ie.ibm.com>
2022-06-15 11:33:59 +0100

    Add goreleaser, and GH actions

--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ var GitCommit string
 //VersionPrerelease is the marker for version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var VersionPrerelease = "beta"
+var VersionPrerelease = "pre"
 
 // SemVersion is an instance of version.Version. This has the secondary
 // benefit of verifying during tests and init time that our version is a
